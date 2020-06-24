068c8574bcb2d64763efacbc8d6e6916b1499145
Rong Zhang <ronozh@gmail.com>
2020-06-24 22:47:41 +1000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+# python setup.py bdist_egg
+setup(name='pyspark-data-analytics',
+      version='0.0.1',
+      description='PySpark Data Analytics Project',
+      url='https://github.com/ronozh/pyspark-data-analytics',
+      author='ronozh',
+      author_email='ronozh@gmail.com',
+      packages=['src'],
+      zip_safe=False)
